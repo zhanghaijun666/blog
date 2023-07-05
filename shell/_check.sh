10c40ab4126407b1f580d0fe55073b67b8d09052
zhanghaijun <dev@bjtxra.com>
2023-07-05 13:21:34 +0800

    优化脚本

--- shell/_check.sh
+++ shell/_check.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-function fun_print() {
-	#printf "%+35s %s\n" `echo -e "$1 \033[1;32m$2\033[0m"`
+function log_print() {
 	echo -e "$1 \033[1;32m\t$2\033[0m"
 }
 
@@ -21,10 +20,12 @@ format_file_size() {
 }
 
 info_cpu() {
-	local cpu_leisure=$(top -b -n 1 | grep Cpu | awk '{print $8}' | cut -f 1 -d "%")
-	fun_print "物理Cpu个数:" "$(sudo cat /proc/cpuinfo | grep "physical id" | uniq | wc -l)"
-	fun_print "系统Cpu核数:" "$(sudo cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $4}')"
-	fun_print "空闲CPU百分比:" "$cpu_leisure%"
+  log_print "CPU名称:" "$(sudo cat /proc/cpuinfo | grep "model name" | awk -F': ' '{print $2}' | uniq)"
+  log_print "CPU架构:" "$(lscpu | grep 'Architecture' | awk '{print $2}' | uniq)"
+	log_print "物理Cpu个数:" "$(sudo cat /proc/cpuinfo | grep "physical id" | uniq | wc -l)"
+	log_print "系统Cpu核数:" "$(sudo cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $4}')"
+  local cpu_leisure=$(top -b -n 1 | grep Cpu | awk '{print $8}' | cut -f 1 -d "%")
+	log_print "空闲CPU百分比:" "$cpu_leisure%"
   local cpu_use=$(printf "%.2f" $(echo "scale=2; 100-$cpu_leisure" | bc))
 	if [ $(echo "${cpu_use} >= 80" | bc) -eq 1 ]; then
 		echo -e "\033[1;31mCPU利用率过高，当前Cpu利用率: $cpu_use% \033[0m"
@@ -32,9 +33,9 @@ info_cpu() {
 }
 
 info_memory() {
-	fun_print "内存总量:" "$(free -h | grep "Mem" | awk '{print $2}')"
-	fun_print "内存空闲:" "$(free -h | grep "Mem" | awk '{print $4}')"
-	fun_print "交换分区使用:" "$(free -h | grep "Swap" | awk '{print $3}')"
+	log_print "内存总量:" "$(free -h | grep "Mem" | awk '{print $2}')"
+	log_print "内存空闲:" "$(free -h | grep "Mem" | awk '{print $4}')"
+	log_print "交换分区使用:" "$(free -h | grep "Swap" | awk '{print $3}')"
 
 	local mem_total_kb=$(free -m | grep "Mem" | awk '{print $2}')
 	local mem_used_kb=$(free -m | grep "Mem" | awk '{print $3}')
@@ -45,11 +46,11 @@ info_memory() {
 }
 
 info_disk() {
-  disk_info=$(lsblk -b)
+  local disk_info=$(lsblk -b)
 
-  fun_print "硬盘数量:" "$(echo "$disk_info" | grep -c 'disk')"
-  fun_print "总空间大小:" "$(format_file_size "$(echo "$disk_info" | grep  'disk' | awk '{ sum += $4 } END { print sum }')")"
-  fun_print "分区数量:" "$(echo "$disk_info" | grep -c 'part')"
+  log_print "硬盘数量:" "$(echo "$disk_info" | grep -c 'disk')"
+  log_print "总空间大小:" "$(format_file_size "$(echo "$disk_info" | grep  'disk' | awk '{ sum += $4 } END { print sum }')")"
+  log_print "分区数量:" "$(echo "$disk_info" | grep -c 'part')"
   # 获取分区信息并输出表头（汉字）
   printf "+------------------+--------+--------+--------+--------+\n"
   printf "| %-20s | %-8s | %-8s | %-8s | %-8s |\n" "文件系统" "总大小" "已使用" "空闲" "使用率"
@@ -58,4 +59,15 @@ info_disk() {
   df -h | awk '$1 ~ /^\/dev/ { printf "| %-16s | %-6s | %-6s | %-6s | %-6s |\n", $1, $2, $3, $4, $5 }'
   # 输出表格底部
   printf "+------------------+--------+--------+--------+--------+\n"
+}
+
+info_os(){
+  log_print "操作系统" "$(cat /etc/os-release | grep 'NAME='|head -n 1 | awk -F '"' '{print $2}')"
+  log_print "操作内核" "$(uname -r)"
+}
+info_network(){
+  local ip=$((ip addr||ifconfig) |grep -v 255.0.0.0|grep -v 255.255.0.0|grep -v docker| sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' |head -1)
+  local network=$(echo $ip|sed  -r "s/([0-9]+\.[0-9]+\.[0-9]+)\..*$/\1.0\/24/g")
+  log_print "IP地址" "$ip"
+  log_print "网关:" "$network"
 }
--- shell/linux_check.sh
+++ shell/linux_check.sh
@@ -8,9 +8,13 @@ set -e
 
 source ./_check.sh
 
+echo -e "\033[1;35m############ 系统信息 ###### `date "+%Y-%m-%d %H:%M:%S"`\033[0m"
+info_os
 echo -e "\033[1;35m############ CPU资源 ###### `date "+%Y-%m-%d %H:%M:%S"`\033[0m"
 info_cpu
 echo -e "\033[1;35m############ 内存资源 ###### `date "+%Y-%m-%d %H:%M:%S"`\033[0m"
 info_memory
 echo -e "\033[1;35m############ 磁盘空间 ###### `date "+%Y-%m-%d %H:%M:%S"`\033[0m"
 info_disk
+echo -e "\033[1;35m############ 网络资源 ###### `date "+%Y-%m-%d %H:%M:%S"`\033[0m"
+info_network
